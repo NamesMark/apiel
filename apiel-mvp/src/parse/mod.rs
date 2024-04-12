127a8a1fc58495bd72b6cfabe54eb5389e180961
morfal <markfirn@gmail.com>
2024-04-12 15:11:02 +0200

    Add debug printouts for lexer

--- apiel-mvp/src/parse/mod.rs
+++ apiel-mvp/src/parse/mod.rs
@@ -4,15 +4,33 @@ use cfgrammar::Span;
 use lrlex::{lrlex_mod, DefaultLexerTypes};
 use lrpar::{lrpar_mod, NonStreamingLexer};
 
-// Include lexer and parser modules
 lrlex_mod!("apiel.l");
 lrpar_mod!("apiel.y");
 
 use apiel_y::Expr;
 
+#[cfg(feature = "debug")]
+use lrlex::DefaultLexeme; 
+#[cfg(feature = "debug")]
+use lrpar::{Lexer, Lexeme};
+
 pub fn parse_and_evaluate(line: &str) -> Result<i64, String> {
     let lexerdef = apiel_l::lexerdef();
     let lexer = lexerdef.lexer(line);
+
+    #[cfg(feature = "debug")]
+    {
+        println!("Tokens:");
+        for tok in lexer.iter() {
+            if let Ok(token) = tok {
+                print!("{} ", token.tok_id());
+            } else {
+                print!("UNKNOWN ");
+            }
+        }
+        println!();
+    }
+
     let (res, errs) = apiel_y::parse(&lexer);
 
     if !errs.is_empty() {
