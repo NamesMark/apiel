7524ae00fbcddcd3cef2b9561be01e3fbb5bfd52
Mark Firman <mark.firman@braiins.cz>
2026-03-26 13:17:55 +0100

    Allow passing arguments into apl! macro

--- apiel/src/macros.rs
+++ apiel/src/macros.rs
@@ -1,9 +1,24 @@
 #[macro_export]
 macro_rules! apl {
+    // Plain expression
     ($expr:expr) => {{
         $crate::parse::parse_and_evaluate($expr)
     }};
+    // Expression with shared environment
     ($expr:expr, $env:expr) => {{
         $crate::parse::parse_and_evaluate_with_env($expr, $env)
+    }};
+    // Monadic: pass right argument (⍵)
+    ($expr:expr, omega: $omega:expr) => {{
+        let mut env = $crate::Env::new();
+        env.insert("⍵".to_string(), $crate::parse::val::Val::from_f64s($omega));
+        $crate::parse::parse_and_evaluate_with_env($expr, &mut env)
+    }};
+    // Dyadic: pass both arguments (⍺ and ⍵)
+    ($expr:expr, alpha: $alpha:expr, omega: $omega:expr) => {{
+        let mut env = $crate::Env::new();
+        env.insert("⍺".to_string(), $crate::parse::val::Val::from_f64s($alpha));
+        env.insert("⍵".to_string(), $crate::parse::val::Val::from_f64s($omega));
+        $crate::parse::parse_and_evaluate_with_env($expr, &mut env)
     }};
 }
